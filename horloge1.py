8e1478105c93000d4ffe30775ddbbfa5f8651ff8
elias-ziane <elias-ziane@github.com>
2023-12-05 09:59:36 +0100

    last changes, work done

--- horloge1.py
+++ horloge1.py
@@ -1,42 +1,27 @@
-from time import strftime, localtime, mktime, sleep
+from time import sleep, strftime, localtime, mktime
+
+alarm_time = None
 
 def afficher_heure():
-    while True:
-        current_time = strftime('%H:%M:%S %p')
-        print(current_time)
+    current_time = strftime('%I:%M:%S %p')
+    print("\r" + current_time, end="", flush=True)
 
-        # Vérifie si l'alarme est définie et si l'heure actuelle correspond à l'heure de l'alarme
-        if alarm_time is not None and strftime('%H:%M:%S') == strftime('%H:%M:%S', localtime(alarm_time)):
-            afficher_message_alarme()
+    if alarm_time is not None and strftime('%I:%M:%S %p') == strftime('%I:%M:%S %p', localtime(alarm_time)):
+        afficher_message_alarme()
 
-        sleep(1)  # Met à jour toutes les 1 seconde
+    sleep(1)
+    afficher_heure()
 
 def regler_alarme(heure_alarme):
     global alarm_time
     alarm_time = mktime((2000, 1, 1, heure_alarme[0], heure_alarme[1], heure_alarme[2], 0, 0, -1))
-    print(f'Alarme réglée à {strftime("%H:%M:%S", localtime(alarm_time))}')
+    print(f'\nALARM SET ON  : {strftime("%I:%M:%S %p", localtime(alarm_time))}')
+
+regler_alarme((8, 51, 00))
 
 def afficher_message_alarme():
-    # Affiche un message lorsque l'heure actuelle correspond à l'heure de l'alarme
-    print("Il est temps !")
+    print("\nCLOCK ! TIME TO WAKE UP BUDDY.")
 
-def get_heure_alarme():
-    # Fonction pour obtenir l'heure de l'alarme sous forme de tuple
-    user_input = input("Entrez l'heure de l'alarme (HH:MM:SS): ")
-
-    # Vérifiez si l'utilisateur a saisi une valeur et si elle est au bon format
-    if user_input and len(user_input.split(':')) == 3:
-        # Convertissez la saisie de l'utilisateur en tuple d'entiers (heures, minutes, secondes)
-        return tuple(map(int, user_input.split(':')))
-    else:
-        # Affichez un message d'erreur si l'entrée est incorrecte
-        print("Format d'heure incorrect. Réglez à nouveau l'alarme.")
-        return None
-
-if __name__ == "__main__":
-    alarm_time = None  # Initialise l'heure de l'alarme à None
-
-    try:
-        afficher_heure()
-    except KeyboardInterrupt:
-        print("\nProgramme terminé.")
+while True:
+    afficher_heure()
+    regler_alarme(())
